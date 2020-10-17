0a38c2d57a9f5a8687e26bcd79c01c4d526309f2
SkillerRaptor <skillerraptor@protonmail.com>
2020-10-17 17:24:18 +0200

    Updating Imports

--- src/HyperDiscord/HyperDiscord.h
+++ src/HyperDiscord/HyperDiscord.h
@@ -2,13 +2,21 @@
 
 /* Core */
 #include "Core/HyperClient.h"
+#include "Core/Token.h"
 
 /* Networking */
+#include "Networking/NetworkClient.h"
 
 /* Objects */
 #include "Objects/Channel.h"
+#include "Objects/Emoji.h"
+#include "Objects/Guild.h"
+#include "Objects/GuildMember.h"
 #include "Objects/Overwrite.h"
+#include "Objects/Role.h"
 #include "Objects/Snowflake.h"
+#include "Objects/VoiceRegion.h"
+#include "Objects/VoiceState.h"
 
 /* Utilities */
 #include "Utilities/Discord.h"
