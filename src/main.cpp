0dc88457c79911b24eaa2fcf9f6ae7babd955d65
SkillerRaptor <skillerraptor@protonmail.com>
2020-11-22 17:24:34 +0100

    [General] Removing Run Call

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,6 @@ using namespace HyperDiscord;
 int main(int argc, char** argv)
 {
 	HyperClient* hyperClient = new HyperClient("Your Token here!", TokenType::BOT);
-	hyperClient->Run();
 	while (true)
 	{
 		hyperClient->Update();
